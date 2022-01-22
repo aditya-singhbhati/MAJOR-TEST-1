c633e6a09b20191922eea633fac92a2c415dbdf0
aditya-singhbhati <98200521+aditya-singhbhati@users.noreply.github.com>
2022-01-22 13:14:44 +0530

    Add files via upload

--- Q2-JAVA-MAJOR-TEST-2.java
+++ Q2-JAVA-MAJOR-TEST-2.java
@@ -1,39 +1,45 @@
-#include<bits/stdc++.h>
-using namespace std;
+import java.util.*;
 
- 
-bool converted (int M, int base)
-{
-  int rem = M % base;
-  M /= base;
-  
-  while (M >= base)
-    {
-      if (M % base != rem)
-	   return 0;
-      
-      M /= base;
-    }
-  
-  if (M == rem)
-    return 1;
-  
-  return 0;
-}
+class Solution
 
-int main ()
 {
-  int M;
-  cin >> M;
-  
-  int base = 2;
-  
-  while (converted (M, base) != 1)
-    {
-      base++;
-    }
-  
-  cout << base;
-  
-  return 0;
+
+	public static boolean convertBase (int m, int base)
+
+	{
+
+		int rem = m % base;
+
+		m = m / base;
+
+		while (m >= base && (m % base == rem))
+
+			m = m / base;
+
+		if (m == rem)
+
+			return true;
+
+		return false;
+	}
+
+	public static void main (String[]args)
+
+	{
+
+		Scanner sc = new Scanner (System.in);
+
+		int m = sc.nextInt ();
+
+		int base = 2;
+
+
+		while (convertBase (m, base) != true)
+
+			base++;
+
+		System.out.println (base);
+
+	}
+
 }
